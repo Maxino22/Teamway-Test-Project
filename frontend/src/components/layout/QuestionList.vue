fd7835ca736a7e6f31d31b647f13f53377eecde2
Maxwell Muhanda <ajaybullec@gmail.com>
2022-10-21 22:40:17 +0300

    refactor tailwindcss code

--- frontend/src/components/layout/QuestionList.vue
+++ frontend/src/components/layout/QuestionList.vue
@@ -1,55 +1,37 @@
 <template>
 	<form class="w-full" @submit.prevent="submitForm">
-		<li
-			class="flex items-center pl-4 rounded border border-gray-200 hover:bg-green-500"
-		>
+		<li class="list-item">
 			<input
 				:id="int"
 				type="radio"
 				:value="introvert"
 				v-model="value"
 				name="answer"
-				class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500"
+				class="radio"
 			/>
-			<label
-				:for="int"
-				class="py-4 ml-2 w-full text-sm font-medium text-gray-900"
-				>{{ int }}</label
-			>
+			<label :for="int" class="label">{{ int }}</label>
 		</li>
-		<li
-			class="flex items-center pl-4 rounded border border-gray-200 hover:bg-green-500"
-		>
+		<li class="list-item">
 			<input
 				:id="ambi"
 				type="radio"
 				:value="ambivert"
 				v-model="value"
 				name="answer"
-				class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500"
+				class="radio"
 			/>
-			<label
-				:for="ambi"
-				class="py-4 ml-2 w-full text-sm font-medium text-gray-900"
-				>{{ ambi }}</label
-			>
+			<label :for="ambi" class="label">{{ ambi }}</label>
 		</li>
-		<li
-			class="flex items-center pl-4 rounded border border-gray-200 hover:bg-green-500"
-		>
+		<li class="list-item">
 			<input
 				:id="extr"
 				type="radio"
 				:value="extrovert"
 				v-model="value"
 				name="answer"
-				class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500"
+				class="radio"
 			/>
-			<label
-				:for="extr"
-				class="py-4 ml-2 w-full text-sm font-medium text-gray-900"
-				>{{ extr }}</label
-			>
+			<label :for="extr" class="label">{{ extr }}</label>
 		</li>
 		<p v-if="error" class="text-sm font-thin text-red-500">
 			Please select one option
